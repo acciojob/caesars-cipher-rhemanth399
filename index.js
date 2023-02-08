5e72efa76086974789eef7beba45e4bb715c6a58
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-02-08 18:03:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,25 @@ const lookup = {
   'Y': 'L','Z': 'M'
 };
 
-function rot13(encodedStr){
+/*function rot13(encodedStr){
   const codeArr = encodedStr.split("");  // String to Array
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
   // your code here
   // Only change code above this line
   return decodedArr.join(""); // Array to String
+}
+*/
+function rot13(str) {
+  let decoded = '';
+  for (let i = 0; i < str.length; i++) {
+    let charCode = str.charCodeAt(i);
+    if (charCode >= 65 && charCode <= 90) {
+      charCode = (charCode - 65 + 13) % 26 + 65;
+    }
+    decoded += String.fromCharCode(charCode);
+  }
+  return decoded;
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
